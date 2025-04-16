48fbdeb882a5e7d69ca1409c43c0a8c5fb1313a2
tmoos94 <168795393+tmoos94@users.noreply.github.com>
2025-04-16 18:42:45 +0900

    0416
    0416

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ new fullpage('#fullpage', {
 
     //    sectionsColor: ['#ff6347', '#4682b4'],
         navigation: true,
-        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection','fifthSection','sixthSection'],
-        navigationTooltips: ['Main', 'Profile', 'Team Project', 'Project', 'Design','Contact'],
+        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection','fifthSection','sixthSection','seventhSection'],
+        navigationTooltips: ['Main', 'Profile', 'Team Project', 'Project', 'Design','Other','Contact'],
     });
+
+//팀프로젝트
+  const box = document.getElementById("imageBox");
+
+  box.addEventListener("mouseenter", () => {
+    box.classList.remove("hover-out");
+    box.classList.add("hover-in");
+  });
+
+  box.addEventListener("mouseleave", () => {
+    box.classList.remove("hover-in");
+    box.classList.add("hover-out");
+  });
