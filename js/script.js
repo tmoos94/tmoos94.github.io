e3f1bcb4affb52b7bec66c62839b2ec0e785014d
tmoos94 <168795393+tmoos94@users.noreply.github.com>
2025-04-19 19:33:49 +0900

    0419
    0419

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
 new fullpage('#fullpage', {
 
-    //    sectionsColor: ['#ff6347', '#4682b4'],
-        navigation: true,
-        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection','fifthSection','sixthSection','seventhSection'],
-        navigationTooltips: ['Main', 'Profile', 'Team Project', 'Project', 'Design','Other','Contact'],
-    });
+  //    sectionsColor: ['#ff6347', '#4682b4'],
+  navigation: true,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection'],
+  navigationTooltips: ['Main', 'Profile', 'Team Project', 'Project', 'Design', 'Other', 'Contact'],
+});
+/*
 
+*/
 //팀프로젝트
-  const box = document.getElementById("imageBox");
+const box = document.getElementById("imageBox");
 
-  box.addEventListener("mouseenter", () => {
-    box.classList.remove("hover-out");
-    box.classList.add("hover-in");
-  });
+box.addEventListener("mouseenter", () => {
+  box.classList.remove("hover-out");
+  box.classList.add("hover-in");
+});
 
-  box.addEventListener("mouseleave", () => {
-    box.classList.remove("hover-in");
-    box.classList.add("hover-out");
-  });
+box.addEventListener("mouseleave", () => {
+  box.classList.remove("hover-in");
+  box.classList.add("hover-out");
+});
